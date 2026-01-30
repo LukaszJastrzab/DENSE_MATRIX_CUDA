9c8153e67bf67cd56b25ced3d25ef23756ad38f7
LucAs Hawk <luk.jastrzab@gmail.com>
2026-01-30 12:32:23 +0100

    householder begins

--- src/dense_matrix_cuda.cuh
+++ src/dense_matrix_cuda.cuh
@@ -20,9 +20,12 @@ class dense_matrix_cuda
 public:
 	/// constructors
 	dense_matrix_cuda() = default;
-	dense_matrix_cuda( const dense_matrix_cuda& ) = default;
+	dense_matrix_cuda( const dense_matrix_cuda& );
 	dense_matrix_cuda( dense_matrix_cuda&& ) = default;
 	dense_matrix_cuda( size_t rows, size_t cols );
+
+	/// destructor
+	~dense_matrix_cuda();
 
 	/// sets matrix sizes and allocates memory
 	void init( size_t rows, size_t cols );
@@ -31,9 +34,18 @@ public:
 	/// adds elements and throws exception if row / col is out of range
 	void set_element( T value, size_t row, size_t col );
 
+	/// decomposes matrix "in situ" to factors QR using Householder method
+	__device__
+	void QR_step( const int step );
+
 private:
 
+	/// divide to upper value
+	__host__ __device__
+	int div_up( int a, int b ) const;
+
 	/// calculates index to flattened matrix element
+	__host__ __device__
 	size_t calc_elem_idx( size_t row, size_t col );
 
 	/// current state of matrix
@@ -43,47 +55,55 @@ private:
 	size_t m_rows{ 0 };
 	/// amount of columns
 	size_t m_cols{ 0 };
+
 	/// flattened matrix data
 	std::vector< T > m_matrix;
 
 	/// for LU decomposition
-	std::vector< T > m_pivots;
+	//std::vector< T > m_pivots;
 	/// for QR decomposition
-	std::vector< T > m_betas;
-	std::vector< T > m_v_firsts;
+	//std::vector< T > m_betas;
+	//std::vector< T > m_v_firsts;
+
+	/// flattened matrix data for GPU device
+	T* m_d_matrix{ nullptr };
+	/// for QU decomposition (on device)
+	T* m_d_betas{ nullptr };
+	T* m_d_v_firsts{ nullptr };
 };
 
 template< typename T >
-struct is_complex : std::false_type {};
-
-template< typename T >
-struct is_complex< thrust::complex< T > > : std::true_type {};
-
-template< typename T >
-struct scalar_type
+__host__ __device__
+int dense_matrix_cuda< T >::div_up( int a, int b ) const
 {
-	using type = T;
-};
-
-template< typename T >
-struct scalar_type< thrust::complex< T > >
-{
-	using type = T;
-};
-
-template< typename T >
-using scalar_t = typename scalar_type< T >::type;
-
-template< typename T >
-size_t dense_matrix_cuda< T >::calc_elem_idx( size_t row, size_t col )
-{
-	return col + row * m_cols;
+    return ( a + b - 1 ) / b;
 }
 
 template< typename T >
 dense_matrix_cuda< T >::dense_matrix_cuda( size_t rows, size_t cols )
 {
 	init( rows, cols );
+}
+
+template< typename T >
+dense_matrix_cuda< T >::dense_matrix_cuda( const dense_matrix_cuda& A )
+	:
+	m_dynamic_state( A.m_dynamic_state ),
+	m_rows( A.m_rows ),
+	m_cols( A.m_cols ),
+	m_matrix( A.m_matrix )
+{
+}
+
+template< typename T >
+dense_matrix_cuda< T >::~dense_matrix_cuda()
+{
+	if( m_d_matrix )
+		cudaFree( m_d_matrix );
+	if( m_d_betas )
+		cudaFree( m_d_betas );
+	if( m_d_v_firsts )
+		cudaFree( m_d_v_firsts );
 }
 
 template< typename T >
@@ -94,6 +114,26 @@ void dense_matrix_cuda< T >::init( size_t rows, size_t cols )
 
 	m_matrix.resize( m_rows * m_cols, T{} );
 }
+
+template< typename T >
+void dense_matrix_cuda< T >::init_cuda()
+{
+	cudaMalloc( &m_d_matrix, m_matrix.size() * sizeof( T ) );
+	cudaMemcpy( m_d_matrix, m_matrix.data(), m_matrix.size() * sizeof( T ), cudaMemcpyHostToDevice );
+
+	auto max_steps = std::min( m_rows - 1, m_cols );
+
+	cudaMalloc( &m_d_betas, max_steps * sizeof( T ) );
+	cudaMalloc( &m_d_v_firsts, max_steps * sizeof( T ) );
+}
+
+template< typename T >
+__host__ __device__
+size_t dense_matrix_cuda< T >::calc_elem_idx( size_t row, size_t col )
+{
+	return col + row * m_cols;
+}
+
 
 template< typename T >
 void dense_matrix_cuda< T >::set_element( T value, size_t row, size_t col )
@@ -107,16 +147,52 @@ void dense_matrix_cuda< T >::set_element( T value, size_t row, size_t col )
 }
 
 template< typename T >
-void dense_matrix_cuda< T >::init_cuda()
+__device__
+void dense_matrix_cuda< T >::QR_step( const int step )
 {
-	if constexpr( is_complex< T >::value )
+	extern __shared__ T sdata[]; // size = blocksize + m_rows
+
+	int tid = threadIdx.x + threadIdx.y * blockDim.x;
+	int block_size = blockDim.x * blockDim.y;
+
+	// first calculate sub column norm
+	// ===============================
+	int col_len = m_rows - step;
+	int col_len_per_thread = div_up( col_len, block_size );
+
+	T sum{};
+
+	int row = step + tid;
+	while( row < m_rows )
 	{
-		auto size_of = sizeof( scalar_t< T > );
-		size_of = size_of;
+		sum += thrust::norm( m_d_matrix[ calc_elem_idx( row, step ) ] );
+		row += block_size;
 	}
-	else
+
+	sdata[ tid ] = sum;
+	__syncthreads();
+
+	// sum up using reduction
+	// ======================
+	for( unsigned int s = block_size / 2; s > 0; s >>= 1 )
 	{
-		auto size_of = sizeof( T );
-		size_of = size_of;
+		if( tid < s )
+			sdata[ tid ] += sdata[ tid + s ];
+		__syncthreads();
 	}
+
+	if( tid == 0 )
+	{
+		T a_kk = m_d_matrix[ calc_elem_idx( step, step ) ];
+		double alpha_abs = std::abs( a_kk );
+		T sign = ( alpha_abs != 0.0 ? -( a_kk ) / alpha_abs : T{ -1 } );
+		T sign_norm = sign * col_norm;
+	}
+
+	__syncthreads();
+
+
+
+
+
 }
--- tests/main.cu
+++ tests/main.cu
@@ -35,3 +35,33 @@ TEST( non_singular_linear_equation_real, QR_decomposition_Householder )
 
 	EXPECT_TRUE( true );
 }
+
+TEST( non_singular_linear_equation_complex, QR_decomposition_Householder )
+{
+	dense_matrix_cuda< thrust::complex< double > > A( MATRIX_ROW_SIZE, MATRIX_COL_SIZE );
+	vector< double > b( MATRIX_ROW_SIZE );
+	vector< double > r( MATRIX_ROW_SIZE );
+	vector< double > x( MATRIX_COL_SIZE );
+
+	for( size_t row{ 0 }; row < MATRIX_ROW_SIZE; ++row )
+	{
+		b[ row ] = generate_random< double >( 0.0001, 10000.0 );
+
+		for( size_t col{ 0 }; col < MATRIX_COL_SIZE; ++col )
+			A.set_element( thrust::complex< double >( generate_random< double >( 0.0001, 10000.0 ),
+				                                      generate_random< double >( 0.0001, 10000.0 ) ),
+				           row, col );
+	}
+
+	A.init_cuda();
+
+	// make a copy for residual vector verification
+	// ============================================
+	//auto A_ = A;
+
+	//A.QR_decomposition();
+	//A.solve_QR( x, b );
+	//A_.count_residual_vector( x, b, r );
+
+	EXPECT_TRUE( true );
+}
